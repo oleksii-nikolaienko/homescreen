e7f69ca338f4030a2318ef85ce5554cdc2214ee9
Oleksii Nikolaienko <oleksii.nikolaienko@gmail.com>
2018-03-22 20:24:59 +0100

    nicer, better

--- fetcher.pl
+++ fetcher.pl
@@ -43,9 +43,22 @@ sub fetchPrecipitation {
     try {
         my $html = $ua->get("https://www.yr.no/en/list/daily/1-92741/Norway/Hordaland/Bergen/Nattland")->decoded_content();
         print file '<html><head><meta http-equiv="refresh" content="'.$tasks{'precipitation'}{'delay'}.'">';
-        print file '<link href=index-48d2f5f87a2b787745b583f7f05d8951.css rel="stylesheet"></head>', "\n<body>";
-        print file $1 if ($html =~ /(<div class="now-timeline-interval__graph-heading">.+?<span>90min<\/span><\/h3><\/div><\/div>)/);
-        print file $1 if ($html =~ /(<div class="now-timeline-interval__text-only">.+?<\/span><\/span><\/div>)/); #.+?<90 minutes
+        #old: print file '<link href=index-48d2f5f87a2b787745b583f7f05d8951.css rel="stylesheet"></head>', "\n<body>";
+        #old: print file $1 if ($html =~ /(<div class="now-timeline-interval__graph-heading">.+?<span>90min<\/span><\/h3><\/div><\/div>)/);
+        #old: print file $1 if ($html =~ /(<div class="now-timeline-interval__text-only">.+?<\/span><\/span><\/div>)/); #.+?<90 minutes
+        
+        #new example: <div><div class="precip precip--rain now-hero__now-precipitation-amount">0<span class="precip__label precip__label--rain precip__label--inline"><abbr title="millimeters">mm</abbr></span></div><span class="now-hero__now-precipitation-text-divider" aria-hidden="true"> - </span><span class="now-hero__now-precipitation-text precipitation-text">No precipitation <span>next hour</span></span></div></div>
+        #new example: <div><div class="precip precip--rain now-hero__now-precipitation-amount">0.1<span class="precip__label precip__label--rain precip__label--inline"><abbr title="millimeters">mm</abbr></span><span class="precip__dash">–</span>0.3<span class="precip__label precip__label--rain precip__label--inline"><abbr title="millimeters">mm</abbr></span></div><span class="now-hero__now-precipitation-text-divider" aria-hidden="true"> - </span><span class="now-hero__now-precipitation-text precipitation-text">Light rain <span>next hour</span></span></div></div>
+        #new example: <div class="now-graph"><div class="nrk-sr">next 30 minutes no precipitation to moderate precipitation, in 30 to 60 minutes no precipitation to moderate precipitation, in 60 to 90 minutes no precipitation to moderate precipitation</div><div aria-hidden="true"><div class="now-graph__y-axis"><svg x="0" y="0" height="25px" focusable="false" width="25px" viewBox="0 0 100 100"><use xlink:href="#icon-precipitation-full" x="0" y="0" width="100" height="100"></use><image src="/assets/images/100/icon-precipitation-full.png" xlink:href></image></svg><svg x="0" y="0" height="25px" focusable="false" width="25px" viewBox="0 0 100 100"><use xlink:href="#icon-precipitation-half" x="0" y="0" width="100" height="100"></use><image src="/assets/images/100/icon-precipitation-half.png" xlink:href></image></svg><svg x="0" y="0" height="25px" focusable="false" width="25px" viewBox="0 0 100 100"><use xlink:href="#icon-precipitation-small" x="0" y="0" width="100" height="100"></use><image src="/assets/images/100/icon-precipitation-small.png" xlink:href></image></svg></div><svg class="now-graph__svg" viewBox="0 0 100 100" preserveAspectRatio="none"><defs><linearGradient id="nowPrecipitationGrad" x1="0%" y1="0%" x2="0%" y2="100%"><stop offset="0%" style="stop-opacity: 1px; stop-color: #0077CC;"></stop><stop offset="100%" style="stop-opacity: 0.4px; stop-color: #0086ff;"></stop></linearGradient></defs><path d="M0 76C1.388888888888889, 76, 5.5555555555555545, 74.66666666666667, 8.333333333333334, 76C11.111111111111112, 77.33333333333333, 13.888888888888888, 80, 16.666666666666668, 84C19.444444444444446, 88, 22.222222222222218, 97.33333333333333, 25, 100C27.777777777777782, 102.66666666666667, 30.555555555555554, 100, 33.333333333333336, 100C36.111111111111114, 100, 38.88888888888889, 100, 41.66666666666667, 100C44.44444444444445, 100, 47.22222222222223, 105.33333333333333, 50, 100C52.77777777777777, 94.66666666666667, 55.55555555555555, 73.33333333333333, 58.333333333333336, 68C61.111111111111114, 62.666666666666664, 63.888888888888886, 68, 66.66666666666667, 68C69.44444444444444, 68, 72.22222222222221, 64, 75, 68C77.77777777777777, 72, 80.55555555555556, 86.66666666666667, 83.33333333333334, 92C86.11111111111113, 97.33333333333333, 88.8888888888889, 98.66666666666667, 91.66666666666667, 100C94.44444444444444, 101.33333333333333, 98.6111111111111, 100, 100, 100 L 100 100 L 0 100 Z" fill="url(#nowPrecipitationGrad)"></path></svg><div class="now-graph__intervals"><span class="now-graph__interval"><span class="now-graph__interval-label">Now</span></span><span class="now-graph__interval"></span><span class="now-graph__interval"><span class="now-graph__interval-label">30</span></span><span class="now-graph__interval"></span><span class="now-graph__interval"><span class="now-graph__interval-label">60</span></span><span class="now-graph__interval"></span><span class="now-graph__interval"><span class="now-graph__interval-label">90</span></span></div></div></div>
+        #ref: http://www.yr.no/place/Norway/Hordaland/Bergen/Nattland/varsel_nu.xml
+        
+        print file '<link href=index-16ce932d0d6d6406e76039f4b8ebb2af.css rel="stylesheet"></head>', "\n<body>";
+        print file '<span class="pulse-icon"></span>';
+        if ($html =~ /(<svg class="now-graph__svg".+?90<\/span><\/span><\/div>)/) {
+            print file '<div class="now-graph" style="width:180px;">', $1, '</div>'
+        } else {
+            print file '&nbsp;&nbsp;No precipitation expected next 90 minutes';
+        }
         print file "</body></html>";
     } catch {
       print file "$_\n";
@@ -86,16 +99,15 @@ sub fetchSchedule {
     my $itemsLimit = 6;
     
     open (file, ">$loc"."schedule.html.new") or die "cannot open schedule.html.new";
+    print file '<html><head><meta http-equiv="refresh" content="'.$tasks{'schedule'}{'delay'}.'">';
+    print file "\n<style>p {margin-top: 0em; margin-bottom: 0em; font-size: small; font-family: monospace; font-size: 12px}</style></head>\n<body>";
     try {
-        print file '<html><head><meta http-equiv="refresh" content="'.$tasks{'schedule'}{'delay'}.'">';
-        #print file '<link href="//fonts.googleapis.com/css?family=Roboto:400,500,600,700,800,900" rel="stylesheet" type="text/css">';
-        print file "\n<style>p {margin-top: 0em; margin-bottom: 0em; font-size: small; font-family: monospace; font-size: 12px}</style></head>\n<body>";
         foreach my $destID (sort keys %destinations) {
             my $response = $ua->get('https://skyss.giantleap.no/public/departures?Hours=12&StopIdentifiers='.$destinations{$destID}{"stopID"})->decoded_content();
             my $json = decode_json $response;
             my (@times, $class, $nItems);
             print file '<p>', '<span style="color:white; font-weight:bold; background-color:black;">', $destinations{$destID}{"line"} , '</span>',
-                '<span style="font-weight:bold">', " → ", $destinations{$destID}{"to"}, "</span>: ";
+                '<span style="font-weight:bold">', " &rarr; ", $destinations{$destID}{"to"}, "</span>: ";
             foreach my $route (@{$$json{"PassingTimes"}}) {
                 my $aimedTime = DateTime->new(
                     year       => $1,
@@ -121,12 +133,12 @@ sub fetchSchedule {
                 last if ++$nItems >= $itemsLimit;
             }
             print file join(", ", @times), "\n";
-        }
-        print file "</body></html>";
+        }    
     } catch {
         print "$_\n";
         print file "$_\n";
     };
+    print file "</body></html>";
     close file;
     rename "$loc"."schedule.html.new", "$loc"."schedule.html";
      
--- homescreen.sh
+++ homescreen.sh
@@ -1,3 +1,6 @@
+killall perl
+killall chromium-browser
 cp -R /home/pi/homescreen /ramdisk/
+export DISPLAY=:0
 chromium-browser --noerrdialogs --disable-infobars --disable-suggestions-service --disable-session-crashed-bubble --incognito --kiosk /ramdisk/homescreen/homescreen.html &
 perl /ramdisk/homescreen/fetcher.pl &
